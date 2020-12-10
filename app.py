9f564747e7bc50594a24da8fa102327dff4881f2
crissymlee <66131888+crissymlee@users.noreply.github.com>
2020-12-09 18:39:26 -0600

    Update flask

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ def index():
     short = 
     return render_template('index.html', short=short)
 
+<<<<<<< Updated upstream
+=======
+def myDropdown():
+
+
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     app.run()
