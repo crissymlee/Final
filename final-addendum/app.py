d556e6ef48c92f2a7db8d8e40382ad375337afd7
crissymlee <66131888+crissymlee@users.noreply.github.com>
2020-12-12 11:50:05 -0600

    Clean up and update to flask

---
+++ final-addendum/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/data', methods=['POST'])
+def data():
+    print(request.form.get('input1'))
+    print(request.form.get('input2'))
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
