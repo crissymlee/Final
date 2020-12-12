b24bf363f54ff5120de1a6d3a93cf2bbb4abe4a5
crissymlee <66131888+crissymlee@users.noreply.github.com>
2020-12-12 10:30:09 -0600

    Update shortest_path.py

--- shortest_path.py
+++ shortest_path.py
@@ -4,80 +4,97 @@
 # In[1]:
 
 
-pip install scikit-network
+from IPython.display import SVG
 
 
 # In[2]:
 
 
-from IPython.display import SVG
+import numpy as np
 
 
 # In[3]:
 
 
-import numpy as np
-
-
-# In[4]:
-
-
-from sknetwork.data import miserables, painters, movie_actor
 from sknetwork.path import shortest_path
 from sknetwork.visualization import svg_graph, svg_digraph, svg_bigraph
 from sknetwork.utils import bipartite2undirected
+from sknetwork.data import load_edge_list, load_graphml, load_netset, load_konect
 
 
 # In[5]:
 
 
-graph = painters(metadata=True)
-adjacency = graph.adjacency
-names = graph.names
-position = graph.position
+import pandas as pd
+import plotly.express as px
 
 
 # In[6]:
 
 
-klimt = 6
-vinci = 9
+graph = load_graphml('Resources/Sprint.graphml')
+adjacency = graph.adjacency
+names = graph.names
+
+
+arr1 = graph.node_attribute.Longitude
+arr2 = graph.node_attribute.Latitude
+position = np.stack((arr1, arr2), axis=1)
 
 
 # In[7]:
 
 
-path = shortest_path(adjacency, sources=klimt, targets=vinci)
+print(graph)
 
 
-# In[8]:
+# In[28]:
+
+
+Seattle = 3
+New_York = 7
+
+
+# In[29]:
+
+
+path = shortest_path(adjacency, sources=Seattle, targets=New_York)
+
+
+# In[30]:
 
 
 edge_labels = [(path[k], path[k + 1], 0) for k in range(len(path) - 1)]
 
 
-# In[9]:
+# In[34]:
 
 
-image = svg_digraph(adjacency, position, names, edge_labels=edge_labels, edge_width=2)
+image = svg_digraph(adjacency, position, names, edge_labels=edge_labels, edge_width=3)
 
 
-# In[10]:
+# In[32]:
 
 
 SVG(image)
 
 
-# In[11]:
+# In[14]:
 
 
-print(graph)
+print(names)
 
 
-# In[12]:
+# In[13]:
 
 
-type(graph)
+print(adjacency)
+
+
+# In[15]:
+
+
+print(position)
 
 
 # In[ ]:
