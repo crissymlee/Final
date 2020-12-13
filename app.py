79651209c44a8f84015b51f149f6139d9643fc91
Stephen Min <stephenkmin@gmail.com>
2020-12-13 16:54:23 -0600

    final push

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, render_template, request
+import attgraph
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/data', methods=['POST', 'GET'])
+def data():
+    input1 = request.form.get('input1')
+    input2 = request.form.get('input2')
+    graphml = attgraph.graphData(input1, input2)
+    
+    return render_template('index.html', graphml = graphml)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
